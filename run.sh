d109635699c97abf6369076807f5405614d6e5a0
Johan Torres <ingeniero204@gmail.com>
2023-11-08 11:15:56 -0500

    "Modificado run.sh"

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 git pull
 sh mvnw clean package
-java -jar -Dserver.port=8080 target/code_net_mongodb-0.0.1-SNAPSHOT.jar
+mkdir -p output
+nohup java -jar -Dserver.port=8080 target/code_net_mongodb-0.0.1-SNAPSHOT.jar > output/logs.log 2>&1 &
+echo $! > pid.txt
