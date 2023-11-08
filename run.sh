12313d287924d54e3878325a4c1c47628c93920c
Johan Torres <ingeniero204@gmail.com>
2023-11-08 11:17:56 -0500

    "Modificado run.sh"

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
+# Kill the service
+kill -9 `cat pid.txt`
+rm pid.txt
+
+# Download and build the service
 git pull
 sh mvnw clean package
+
+# Run the service
 mkdir -p output
 nohup java -jar -Dserver.port=8080 target/code_net_mongodb-0.0.1-SNAPSHOT.jar > output/logs.log 2>&1 &
 echo $! > pid.txt
